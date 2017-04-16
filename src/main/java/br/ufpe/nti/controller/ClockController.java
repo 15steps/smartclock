cc351b877111cdf2cc1bb8c280652e54222e1726
Wellington Felix <wellfelix@Wellingtons-iMac.local>
2017-04-15 22:27:59 -0300

    Methods in ClockController refactored to ClockService. Database connection working

--- src/main/java/br/ufpe/nti/controller/ClockController.java
+++ src/main/java/br/ufpe/nti/controller/ClockController.java
@@ -1,93 +1,47 @@
 package br.ufpe.nti.controller;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.ufpe.nti.controller.repository.ClockHistoryRepository;
 import br.ufpe.nti.model.Clock;
+import br.ufpe.nti.service.ClockService;
 
 @RestController
 public class ClockController {
-		
+	
+	@Autowired
+	private ClockHistoryRepository db;
+	@Autowired
+	private ClockService clockService;
+	
 	@RequestMapping(value = "/clock", method = RequestMethod.GET)
 	public ResponseEntity<String> GetAngleRequest() {
 		Clock clock = new Clock();		
-		double angle = computeAngle(clock.getTime());
+		double angle = clockService.computeAngle(clock.getTime());
 		
-		return getClockResponse(clock, angle);
+		return clockService.getClockResponse(clock, angle);
 	}
 	
 	@RequestMapping(value = "/clock", method = RequestMethod.POST)
 	public ResponseEntity<String> PostAngleRequest(@RequestBody String body) {
 		Clock clock = new Clock();
 		double angle = 0.0;
-		try {
-			JSONObject jsonBody = new JSONObject(body);
-			LocalTime lt = LocalTime.parse(jsonBody.getString("time"));
-			clock.setTime(lt);
-			angle = computeAngle(lt);
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return getClockResponse(clock, angle);
-	}
-
-	/**
-	 * @param clock
-	 * @param angle
-	 * @return complete HTTP response
-	 */
-	private ResponseEntity<String> getClockResponse(Clock clock, double angle) {
-		HttpHeaders header = new HttpHeaders();
-		header.setContentType(MediaType.APPLICATION_JSON);
-		DateFormat createdAtFormatter = new SimpleDateFormat("YYYY-MM-DD HH:mm:ss");
 		
-		JSONObject body = new JSONObject();
-		try {
-			body.put("id", "null");
-			body.put("time", clock.getTime().format(DateTimeFormatter.ofPattern("HH:mm")));
-			body.put("createdAt", createdAtFormatter.format(clock.getCreatedAt()));
-			body.put("angle", angle);
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		LocalTime lt = clockService.jsonToLocalTime(body);
+		clock.setTime(lt);
+		angle = clockService.computeAngle(lt);
 		
-		return new ResponseEntity<String>(body.toString(), header, HttpStatus.OK);
-	}
-	
-	/**
-	 * @param time
-	 * @return angle between clock hands
-	 */
-	private double computeAngle(LocalTime time) {
-		double angle = 0.0;
-		int hour = time.getHour();
-		int minute = time.getMinute();
+		//database insertion
+		db.save(clock);
 		
-		hour = hour > 12 ? hour - 12 : hour;
-		
-		double dh = (60.0*hour + minute)/2.0;
-		double dm = 6.0*minute;
-		
-		//angle = Math.abs(0.5*(60*(hour-11)*minute));
-		angle = Math.abs(dh - dm);
-		angle = angle > 180.0 ? 360.0 - angle : angle;
-		
-		return angle;
+		return clockService.getClockResponse(clock, angle);
 	}
 	
 }
---
+++ src/main/java/br/ufpe/nti/service/ClockService.java
@@ -0,0 +1,80 @@
+package br.ufpe.nti.service;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import br.ufpe.nti.model.Clock;
+
+@Service
+public class ClockService {
+	
+	/**
+	 * @param clock
+	 * @param angle
+	 * @return complete HTTP response
+	 */
+	public ResponseEntity<String> getClockResponse(Clock clock, double angle) {
+		HttpHeaders header = new HttpHeaders();
+		header.setContentType(MediaType.APPLICATION_JSON);
+		DateFormat createdAtFormatter = new SimpleDateFormat("YYYY-MM-DD HH:mm:ss");
+		
+		JSONObject body = new JSONObject();
+		try {
+			body.put("id", "null");
+			body.put("time", clock.getTime().format(DateTimeFormatter.ofPattern("HH:mm")));
+			body.put("createdAt", createdAtFormatter.format(clock.getCreatedAt()));
+			body.put("angle", angle);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return new ResponseEntity<String>(body.toString(), header, HttpStatus.OK);
+	}
+	
+	/**
+	 * @param time
+	 * @return angle between clock hands
+	 */
+	public double computeAngle(LocalTime time) {
+		double angle = 0.0;
+		int hour = time.getHour();
+		int minute = time.getMinute();
+		
+		hour = hour > 12 ? hour - 12 : hour;
+		
+		double dh = (60.0*hour + minute)/2.0;
+		double dm = 6.0*minute;
+		
+		angle = Math.abs(dh - dm);
+		angle = angle > 180.0 ? 360.0 - angle : angle;
+		
+		return angle;
+	}
+	
+	/**
+	 * @param body
+	 * @return LocalTime from json body
+	 */
+	public LocalTime jsonToLocalTime(String body) {
+		LocalTime lt = LocalTime.MIDNIGHT;
+		try {
+			JSONObject jsonBody = new JSONObject(body);
+			lt = LocalTime.parse(jsonBody.getString("time"));
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return lt;
+	}
+}
